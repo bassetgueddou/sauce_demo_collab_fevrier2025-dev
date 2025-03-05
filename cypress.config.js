1672ec12d5e360f8ff517d5924dedea8ce72251d
bassetgueddou <bassetgueddou@gmail.com>
2025-03-05 13:52:53 +0100

    add config to generate report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here 
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) { 
+      on("file:preprocessor", cucumber());
+      require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
     },
   },
 });
